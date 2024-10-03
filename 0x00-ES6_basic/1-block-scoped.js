2024329bb1e8251fc32622279d01f982e0ebe2bf
Dev-Kings <kingasiadavid41@gmail.com>
2024-10-03 18:05:05 +0300

    fixed let and const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
+const task = false;
+const task2 = true;
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
 
   if (trueOrFalse) {
     let task = true;
