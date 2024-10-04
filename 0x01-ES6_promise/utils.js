8ccc76582170daa9f70ca8112cec94fcb15b106b
Dev-Kings <kingasiadavid41@gmail.com>
2024-10-04 11:30:39 +0300

    Promise instance

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,13 @@
+export function uploadPhoto() {
+  return Promise.reolve({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+export function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}
