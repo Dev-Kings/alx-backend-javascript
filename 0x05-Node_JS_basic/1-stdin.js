dbd469a168ac1198e0b8514f91f11eba57122f33
Dev-Kings <kingasiadavid41@gmail.com>
2024-11-26 19:37:36 +0300

    ref4

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,42 +1,14 @@
-// const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// // Create an interface to read input from the command line
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-// // Display the message asking for the user's name
-// rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-//   // Output the name entered by the user
-//   console.log(`Your name is: ${name}`);
-
-//   // Close the readline interface
-//   rl.close();
-
-//   // Check if the input is coming from a piped source (not interactive)
-//   if (!process.stdin.isTTY) {
-//     console.log('This important software is now closing');
-//   }
-// });
-
-const readline = require('readline');
-
-// Create a readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-// Ask for the user's name
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-
-  // Only print the closing message if the input was piped, not interactive
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-
-  // Close the readline interface
-  rl.close();
+/* 'end' event occurs when there is no more data to be read from the input stream */
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
